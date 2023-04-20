83162ce1f992a074b64218c7311daa2f719553d4
caio.rodrigo.santos <caio.rodrigo.santos@accenture.com>
2023-04-20 10:33:50 -0300

    chamando api e listando items+tempo

--- apirequests/stratz.py
+++ apirequests/stratz.py
@@ -6,6 +6,21 @@ from apirequests.configstratz import BASE_URL, headers
 responseitem = requests.get(f"{BASE_URL}/Item", headers=headers, verify=False)
 item_data = responseitem.json()
 
+core_items = ['Falcon Blade', 'Power Treads', 'Mask of Madness', 'Boots of Travel', 'Witch Blade',
+              'Eul\'s Scepter of Divinity',
+              'Orchid Malevolence', 'Aghanim\'s Scepter', 'Refresher Orb', 'Octarine Core', 'Scythe of Vyse',
+              'Aghanim\'s Blessing',
+              'Gleipnir, Wind Waker', 'Hood of Defiance', 'Blade Mail', 'Eternal Shroud', 'Lotus Orb', 'Black King Bar',
+              'Bloodstone',
+              'Hurricane Pike', 'Linken\'s Sphere', 'Manta Style', 'Shiva\'s Guard', 'Heart of Tarrasque',
+              'Assault Cuirass',
+              'Meteor Hammer', 'Armlet of Mordiggian', 'Skull Basher', 'Shadow Blade', 'Desolator', 'Battle Fury',
+              'Nullifier',
+              'Ethereal Blade', 'Radiance', 'Butterfly', 'Monkey King Bar', 'Daedalus', 'Silver Edge', 'Divine Rapier',
+              'Revenant\'s Brooch', 'Abyssal Blade', 'Bloodthorn', 'Dragon Lance', 'Mage Slayer', 'Diffusal Blade',
+              'Echo Sabre', 'Maelstrom', 'Heaven\'s Halberd', 'Kaya and Sange', 'Sange and Yasha', 'Yasha and Kaya',
+              'Satanic', 'Eye of Skadi', 'Mjollnir', 'Arcane Blink', 'Overwhelming Blink', 'Swift Blink']
+
 
 def search_match(player_id, hero_id):
     response = requests.get(f"{BASE_URL}/player/{player_id}/matches?take=30", headers=headers,
@@ -19,15 +34,34 @@ def search_match(player_id, hero_id):
                     return get_items_per_time(match['id'], player_id)
 
 
+def check_if_core(_item):
+    for item in core_items:
+        if _item == item:
+            return item
+
+
+def format_text_to_display(items_n_timing):
+    formated_items = ''
+    for index, item in enumerate(items_n_timing):
+        if index % 2 == 0 and item != '':
+            formated_items += item + " - "
+
+        elif item != '':
+            formated_items += item + ' \n'
+
+    return formated_items
+
+
 def get_items_per_time(matchid, playerid):
     responsematch = requests.get(f"{BASE_URL}/match/{matchid}/breakdown", headers=headers, verify=False)
     data = responsematch.json()
-    items_n_timing = ('', '')
+    items_n_timing = ()
     for players in data['players']:
         if players['steamAccountId'] == playerid:
             for items in players['stats']['itemPurchases']:
-                items_n_timing += (str(datetime.timedelta(seconds=items['time'])), get_name_by_id(items['itemId']))
-            return items_n_timing
+                if check_if_core(get_name_by_id(items['itemId'])):
+                    items_n_timing += (str(datetime.timedelta(seconds=items['time'])), get_name_by_id(items['itemId']))
+            return format_text_to_display(items_n_timing)
 
 
 def get_name_by_id(search_id):
--- overlay/overlayfunctions.py
+++ overlay/overlayfunctions.py
@@ -7,14 +7,6 @@ def inicia_overlay(imagem_path, root, label, hero_id, player_id):
     posicao = pyautogui.locateOnScreen(imagem_path, region=regiao_busca, grayscale=True)
 
     if posicao:
-        # print(posicao)
-        # label['text'] = '1 - Agrar creeps na lane (treinar esse conceito & last hit)\n2 - Farmar o little camp sempre ' \
-        #                 'que possível e ir farmando a jungle na side lane\n3- Caso a lane esteja dificil e tenha o item ' \
-        #                 'de farm, farmar ancient + lane do top\n4- Caso a primeira torre do top tiver caído, ' \
-        #                 'farmar jungle do time inimigo, caso não, cogitar farmar propria jungle\n5- Wardar a jungle ' \
-        #                 'inimiga'
         label['text'] = search_match(player_id, hero_id)
-        # str(datetime.timedelta(seconds=items['time'])), get_name_by_id(items['itemId'])
-
     else:
         root.after(1, inicia_overlay(imagem_path, root, label, hero_id, player_id))
